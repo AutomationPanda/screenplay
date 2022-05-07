ea06f120070e49acba8769c306885acf9b008ef1
Pandy Knight <andylpk.247@gmail.com>
2022-05-07 19:11:34 -0400

    screenplay 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
   name="screenplay",
-  version="0.1.2",
+  version="0.1.3",
   author="Pandy Knight",
   author_email="",
   description="The Python Screenplay Pattern",
